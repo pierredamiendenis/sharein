01d1c0df99f7cfa72ad20d2a336ff80d22aedfb9
cocoricozozo <florian.letoumelin@gmail.com>
2019-03-21 12:01:59 +0100

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+import { Routes, RouterModule } from '@angular/router';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    //RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
